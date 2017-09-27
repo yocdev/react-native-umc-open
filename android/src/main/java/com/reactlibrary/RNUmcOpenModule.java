5e19b86ed30c265a3b8fe4fe74df7d3f1c7b3563
JianMing Zhong <windlessg@gmail.com>
2017-09-27 16:08:26 +0800

    login method

--- android/src/main/java/com/reactlibrary/RNUmcOpenModule.java
+++ android/src/main/java/com/reactlibrary/RNUmcOpenModule.java
@@ -5,14 +5,22 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import cm.pass.sdk.UMCSDK;
+import cm.pass.sdk.auth.AuthnHelper;
+import cm.pass.sdk.auth.TokenListener;
+import org.json.JSONObject;
 
 public class RNUmcOpenModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
+  private AuthnHelper authHelper;
+
   public RNUmcOpenModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+
+    authHelper = AuthHelper.getInstance(reactContext);
   }
 
   @Override
@@ -21,7 +29,15 @@ public class RNUmcOpenModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void login(String appId, String appKey) {
-      return "OK"
+  public void login(String appId, String appKey, Callback callback) {
+    TokenListener listener = new TokenListener() {
+      @Override
+      public void onGetTokenComplete(JSONObject json) {
+        callback.invoke(json.toString());
+      }
+    };
+    authnHelper.umcLoginByType(
+        appId, appKey,
+        AuthnHelper.UMC_LOGIN_DISPLAY, listener);
   }
 }
