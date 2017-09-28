364a90076e3d7d3ce5d517c351e2ad5f10e4daf0
JianMing Zhong <windlessg@gmail.com>
2017-09-28 18:03:22 +0800

    api

--- android/src/main/java/com/reactlibrary/RNUmcOpenModule.java
+++ android/src/main/java/com/reactlibrary/RNUmcOpenModule.java
@@ -1,39 +1,38 @@
 
 package com.reactlibrary;
 
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
-import cm.pass.sdk.UMCSDK;
-import cm.pass.sdk.auth.AuthnHelper;
-import cm.pass.sdk.auth.TokenListener;
-import sun.reflect.ReflectionFactory.GetReflectionFactoryAction;
 
 import org.json.JSONObject;
 
+import cm.pass.sdk.auth.AuthnHelper;
+import cm.pass.sdk.auth.TokenListener;
+
 public class RNUmcOpenModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNUmcOpenModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNUmcOpenModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNUmcOpen";
-  }
+    @Override
+    public String getName() {
+        return "RNUmcOpen";
+    }
 
-  @ReactMethod
-  public void login(String appId, String appKey, final Callback callback) {
-    AuthnHelper.getInstance(getReactApplicationContext())
-        .umcLoginByType(getReactApplicationContext(), appId, appKey, AuthnHelper.UMC_LOGIN_DISPLAY, new TokenListener() {
-          @Override
-          public void onGetTokenComplete(JSONObject jsonObject) {
-            callback.invoke(jsonObject.toString());
-          }
-        });
-  }
+    @ReactMethod
+    public void login(String appId, String appKey, final Callback callback) {
+        AuthnHelper.getInstance(getReactApplicationContext())
+                .umcLoginByType(appId, appKey, AuthnHelper.UMC_LOGIN_DISPLAY, new TokenListener() {
+                    @Override
+                    public void onGetTokenComplete(JSONObject jsonObject) {
+                        callback.invoke(jsonObject.toString());
+                    }
+                }, false);
+    }
 }
